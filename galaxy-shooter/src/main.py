e1ee30f83f92faf2d189ac539aa6f430713afaed
Akshay Reddy <amreddy@e-idsolutions.com>
2021-03-12 19:35:06 -0500

    enemy has got the bullets

--- galaxy-shooter/src/main.py
+++ galaxy-shooter/src/main.py
@@ -1,5 +1,6 @@
 
 import pygame, random, math
+from datetime import datetime
 
 # initialize pygame
 pygame.init()
@@ -7,9 +8,15 @@ pygame.init()
 # screen size
 gameScreenX = 800
 gameScreenY = 600
+numberOfEnemies = 3
 
 screen = pygame.display.set_mode((gameScreenX, gameScreenY))
 scoreFont = pygame.font.Font('freesansbold.ttf', 32)
+
+# background music
+# Lobo Loco - Insterstellar Icebreaker (ID 1446)
+pygame.mixer.music.load('background.mp3')
+pygame.mixer.music.play(-1)
 
 # game title
 pygame.display.set_caption("Galaxy Shooter")
@@ -43,11 +50,13 @@ class Spaceship:
 
     # fire a bullet
     def fire(self):
-        bullet = Bullet(self.positionX, self.positionY)
+        pygame.mixer.Sound('Gun+Silencer.wav').play()
+        bullet = SpaceshipBullet(self.positionX, self.positionY)
         self.bullets.append(bullet)
 
 class Enemy:
     stepSize = 0.4
+    canFireTime = random.randint(4, 6) 
 
     def __init__(self):
         self.bodyX = 50
@@ -57,6 +66,8 @@ class Enemy:
         self.enemyMovement = 'left'
         self.positionX = random.randint(50, gameScreenX - 50)
         self.positionY = random.randint(0, 50)
+        self.lastBulletFiredTime = datetime.now()
+        self.bullets = []
 
 
     def move(self):
@@ -77,6 +88,22 @@ class Enemy:
     def destroyed(self):
         self.body  = pygame.transform.scale(self.body, (10, 10))
 
+    def canFire(self):
+        # check if the enemy can fire 
+        currentTime = datetime.now()
+        if (currentTime - self.lastBulletFiredTime).total_seconds() > self.canFireTime:
+            return True
+        else:
+            return False
+    
+    # fire a bullet
+    def fire(self):
+        if self.canFire():
+            pygame.mixer.Sound('Gun+Silencer.wav').play()
+            bullet = EnemyBullet(self.positionX, self.positionY)
+            self.lastBulletFiredTime = datetime.now()
+            self.bullets.append(bullet)
+
 
 class Bullet:
     stepSize = 1
@@ -92,10 +119,18 @@ class Bullet:
     def addToScreen(self):
         screen.blit(self.body, (self.positionX, self.positionY))
 
+class SpaceshipBullet(Bullet):
     # move towards the enemy
     def move(self):
         self.positionY = self.positionY - self.stepSize
         self.addToScreen()
+
+class EnemyBullet(Bullet):
+    # move towards the spaceship
+    def move(self):
+        self.positionY = self.positionY + self.stepSize
+        self.addToScreen()
+
 
 def getNewBackgroundColor():
     r = random.randint(20, 30)
@@ -146,11 +181,22 @@ while running:
     spaceship.addToScreen()
 
     # ensure min number of enemies
-    if len(enemies) < 6:
+    if len(enemies) < numberOfEnemies:
         enemies.append(Enemy())
     
+
     for enemy in enemies:
         enemy.move()
+        distanceToSpaceship = euclidianDistance(enemy.positionX, spaceship.positionX, 
+                                                enemy.positionY, spaceship.positionY)
+        if distanceToSpaceship < 500:
+            enemy.fire()
+
+        for bullet in enemy.bullets:
+            bullet.move()
+            if bullet.positionY > gameScreenY:
+                enemy.bullets.remove(bullet)
+        
 
     # fire the bullets and remove when out of the game space
     for bullet in spaceship.bullets:
@@ -160,13 +206,15 @@ while running:
         
         # if the bullet hits any enemy
         for enemy in enemies:
-            hitDistance = euclidianDistance(enemy.positionX, bullet.positionX, enemy.positionY, bullet.positionY)
+            hitDistance = euclidianDistance(enemy.positionX, bullet.positionX, 
+                                            enemy.positionY, bullet.positionY)
             if hitDistance < 30:
+                pygame.mixer.Sound('Gun+Reload.wav').play()
                 enemies.remove(enemy)
                 spaceship.score = spaceship.score + 1
     
     showScore(spaceship.score)
-        
     pygame.display.update()
+    # pygame.event.clear()
 
         
